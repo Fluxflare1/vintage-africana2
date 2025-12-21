c96ef9446630bfd193388e189b38136c5fd29ae2
Itebimien Momodu <fluxflareltd@gmail.com>
2025-12-21 13:27:26 +0100

    Create page.tsx

---
+++ frontend/app/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>Vintage Africana</h1>
+      <p>Frontend foundation is configured.</p>
+    </main>
+  );
+}

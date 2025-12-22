c0907b19092dfe06db4927006b7b41493c4447bb
Itebimien Momodu <fluxflareltd@gmail.com>
2025-12-22 16:42:57 +0100

    Create page.tsx

---
+++ frontend/app/admin/pages/edit/page.tsx
@@ -0,0 +1,17 @@
+import StatusBar from "@/components/admin/StatusBar";
+import { useState } from "react";
+
+export default function PageEditor() {
+  const [page, setPage] = useState({
+    status: "draft",
+  });
+
+  return (
+    <>
+      <StatusBar
+        status={page.status}
+        setStatus={(v) => setPage({ ...page, status: v })}
+      />
+    </>
+  );
+}

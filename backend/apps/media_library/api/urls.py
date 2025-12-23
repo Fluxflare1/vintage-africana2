3fa12ace45e5b6a243092fea160da377ec722aa9
Itebimien Momodu <fluxflareltd@gmail.com>
2025-12-23 14:24:36 +0100

    Create urls.py

---
+++ backend/apps/media_library/api/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import admin_media_list, admin_media_upload
+
+urlpatterns = [
+    path("admin/media/", admin_media_list, name="admin_media_list"),
+    path("admin/media/upload/", admin_media_upload, name="admin_media_upload"),
+]
